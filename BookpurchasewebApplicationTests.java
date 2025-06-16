0f9c8b63ba973590bc2e2ccf4512871d2a6cf5c1
rufinmlili <lilianrufinmartinez@gmail.com>
2025-06-16 22:20:55 +0200

    Add files via upload

---
+++ BookpurchasewebApplicationTests.java
@@ -0,0 +1,13 @@
+package progkorny.bookpurchaseweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookpurchasewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
