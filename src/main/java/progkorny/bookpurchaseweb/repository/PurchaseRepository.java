c59fd7327d291136ec748bc288340ad063dae292
rufinmlili <lilianrufinmartinez@gmail.com>
2025-06-23 14:16:51 +0200

    Update PurchaseRepository.java

--- src/main/java/progkorny/bookpurchaseweb/repository/PurchaseRepository.java
+++ src/main/java/progkorny/bookpurchaseweb/repository/PurchaseRepository.java
@@ -3,11 +3,11 @@ package progkorny.bookpurchaseweb.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import progkorny.bookpurchaseweb.model.Purchase;
 
-// Ez az interfész a RentalEvent entitás adatbázis műveleteit kezeli.
+// Ez az interfész a Purchase entitás adatbázis műveleteit kezeli.
 // A JpaRepository által biztosított CRUD műveleteket használja,
 // így nincs szükség manuális implementációra.
 public interface PurchaseRepository extends JpaRepository<Purchase, Integer> {
     // Paraméterek:
-    // RentalEvent: az entitás típusa, amelyhez a repository tartozik
-    // Integer: a RentalEvent entitás azonosítójának típusa (id mező típusa)
+    // Purchase: az entitás típusa, amelyhez a repository tartozik
+    // Integer: a Purchase entitás azonosítójának típusa (id mező típusa)
 }
